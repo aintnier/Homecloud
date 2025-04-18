c28ef20a21e0177f57ce7b6b64a0ede2d88c0925
aintnier <aintnier@gmail.com>
2025-04-18 23:15:59 +0200

    Implementazione interfaccia UI/UX della dashboard

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import AddDeadline from "./pages/AddDeadline";
 import DeadlineDetails from "./pages/DeadlineDetails";
 import UserProfile from "./pages/UserProfile";
 import ErrorPage from "./pages/ErrorPage";
+import Test from "./pages/Test";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/add-deadline" element={<AddDeadline />} />
-        <Route path="/deadline-details" element={<DeadlineDetails />} />
+        <Route path="/deadline-details/:id" element={<DeadlineDetails />} />
         <Route path="/profile" element={<UserProfile />} />
         <Route path="*" element={<ErrorPage />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   );
--- src/pages/DeadlineDetails.js
+++ src/pages/DeadlineDetails.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const DeadlineDetails = ({ match }) => {
-  const { id } = match.params;
+const DeadlineDetails = () => {
+  const { id } = useParams();
 
   return (
     <div>
-      <h1>Dettagli Scadenza: {id}</h1>
-      <p>Visualizza i dettagli della scadenza qui.</p>
+      <h1>Dettagli Scadenza</h1>
+      <p>ID Scadenza: {id}</p>
+      {/* Qui puoi aggiungere il codice per recuperare e mostrare i dettagli della scadenza */}
     </div>
   );
 };
