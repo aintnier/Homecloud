ebfe7c3d4cd5fd7d25c534c584ae5b8bf76c213f
Ernesto Cervadoro <aintnier@gmail.com>
2025-07-25 17:50:24 +0200

    Removed FuzzyText component from Error Page and fixed responsivness issues

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,20 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/ErrorPage.css";
-import FuzzyText from "../components/FuzzyText/FuzzyText.jsx";
 
 const ErrorPage = () => {
   return (
     <div className="error-page">
-      <FuzzyText
-        baseIntensity={0.2}
-        hoverIntensity={0.4}
-        enableHover={true}
-        fontWeight={900}
-        color="#ffffff"
-      >
-        404
-      </FuzzyText>
+      <h1 className="error-title">404</h1>
       <p>Oops! La pagina che stai cercando non esiste.</p>
       <Link to="/" className="back-button">
         Torna alla Home
